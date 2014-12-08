8725d57e47e52fe3298c568697e45e877fe038d3
Devonte Applewhite <dapplewh@nd.edu>
2014-12-08 02:22:02 -0500

    Modified m.sh and made solver multithreaded.

--- m.sh
+++ m.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc oracle.c -o oracle -lcrypto -Wall
-gcc solver.c -o solver -lcrypto -Wall
+gcc solver.c -o solver -lcrypto -lpthread -Wall
--- solver.c
+++ solver.c
@@ -6,12 +6,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-//#include <time.h> //for timing purposes
+#include <pthread.h>
 #include <limits.h>
 #include <openssl/des.h>
 #include <openssl/rand.h>
 
 #define BLOCK 8 //size in bytes of the plaintext and ciphertext
+
+struct targs{
+	unsigned int tnum;
+	unsigned long int start;
+	unsigned long int iterations;
+};
+
+unsigned char plainbuff[8][BLOCK],cipherbuff[8][BLOCK];//,check[BLOCK];
 
 //reverses an array to get true converted number value
 void revuchararr(unsigned char a[8]){
@@ -71,21 +79,44 @@ unsigned long int uchararray2lu(unsigned char a[], int size){
 	return ans;
 }
 
-int main(int argc, char *argv[]){
-	if(argc != 5){ //check for all arguments
-		printf("Usage:%s <plaintext_file> <ciphertext_file <key_start> <iterations>\n",argv[0]);
-		return 1; //exit failure
-	}
-
-	int i;
-	//buffers for plaintext, ciphertext, and the equality check
-	unsigned char plainbuff[BLOCK],cipherbuff[BLOCK],check[BLOCK];
+void * find_key(struct targs *args)
+{
+	unsigned char check[BLOCK]; //for checking the answer
 	DES_cblock key; //the cblock key
 	DES_key_schedule keysched; //for key scheduling
 
+	memset(check, 0, sizeof(*check)*BLOCK);
+
+	unsigned long int start = args->start; //get starting value
+	unsigned long int iterations = args->iterations; //number of iterations
+	unsigned int i = args->tnum;
+	unsigned long int j;
+
+	for(j=0;j<iterations;j++){
+		num2uchararray(start+j,key); //turn number into actual key
+		DES_set_odd_parity(&key); //set odd parity on key
+		DES_set_key((C_Block *)key, &keysched); //expand the key for speed
+		DES_ecb_encrypt((C_Block *)cipherbuff[i],(C_Block *)check, &keysched, DES_DECRYPT); //decrypt the block with the current key being checked
+		if(isequaluchararray(plainbuff[i],check,BLOCK)){ //checking equality
+			unsigned char *ans = malloc(sizeof(unsigned char)*BLOCK);
+			memcpy(ans,key,BLOCK); //copy over answer
+			return ans; //return answer
+		}
+	}
+
+	return NULL; //answer not found
+}
+
+int main(int argc, char *argv[]){
+	if(argc != 6){ //check for all arguments
+		printf("Usage:%s <plaintext_file> <ciphertext_file <key_start> <iterations> <nthreads>\n",argv[0]);
+		return 1; //exit failure
+	}
+
+	unsigned int i;
+
 	memset(plainbuff, 0, sizeof(*plainbuff)); //zero the buffers
 	memset(cipherbuff, 0, sizeof(*cipherbuff));
-	memset(check, 0, sizeof(*check));
 
 	int plaintextfd,ciphertextfd; //read and write file descriptors
 	if((plaintextfd = open(argv[1],O_RDONLY)) == -1){ //failed to open
@@ -115,46 +146,74 @@ int main(int argc, char *argv[]){
 		return 1; //exit failure
 	}
 
+	for(i=1;i<8;i++){ //copy all info from 0th buffer to 7th buffer
+		memcpy(plainbuff[i],plainbuff[0],BLOCK);
+		memcpy(cipherbuff[i],cipherbuff[0],BLOCK);
+	}
+
 	unsigned long int start = atol(argv[3]); //get starting value
 	unsigned long int iterations = atol(argv[4]); //get number of iterations
-	unsigned long int j;
+	unsigned int nthreads = atoi(argv[5]); //get number of threads
 
-	//unsigned char randkey[8]; //random key to work with
-	//memset(randkey,0,sizeof(*randkey)*8); //zero the random key
+	struct targs *allargs = malloc(sizeof(struct targs)*nthreads);
+	pthread_t *threads = malloc(sizeof(pthread_t)*nthreads); //malloc threads
+
+	unsigned long int div;
+	int dodiff = 0;
+	if(iterations%nthreads == 0){ //perfect divide
+		div = iterations/nthreads;
+	}else{
+		div = iterations/nthreads + 1; //account for the extra slack
+		dodiff = 1;
+	}
+
+	for(i=0;i<nthreads;i++){ //set up arguments and dispatch threads
+		allargs[i].tnum = i; //thread number
+		allargs[i].start = start+div*i; //set up starting point
+		if(i != nthreads-1){ //not ending thread
+			allargs[i].iterations = div;
+		}else{
+			if(dodiff){
+				allargs[i].iterations = iterations%div; //ending work
+			}else{
+				allargs[i].iterations = div; //ending work
+			}
+		}
+
+		int st;
+		//keep dispatching a particular thread until it actually succeeds
+		while((st=pthread_create(&threads[i],NULL,(void *)&find_key,&allargs[i]))!=0){
+			//printf("Thread failed to create: %s\n",strerror(errno));
+		}
+	}
 
 	unsigned long int value = -1; //the correct key
-	for(j=0;j<iterations;j++){
-		//memset(randkey,0,sizeof(*randkey)*8); //zero random key
-		num2uchararray(start+j,key); //turn number into actual key
-		//for(i=0;i<8;i++){ //copy the number into the key.
-		//	key[i] = randkey[i]; //put values in the key structure
-		//}
-		DES_set_odd_parity(&key); //set odd parity on key
-		DES_set_key((C_Block *)key, &keysched); //expand the key for speed
-		DES_ecb_encrypt((C_Block *)cipherbuff,(C_Block *)check, &keysched, DES_DECRYPT); //decrypt the block with the current key being checked
-		if(isequaluchararray(plainbuff,check,BLOCK)){ //checking equality
-			value = start+j;
-			//printf("randkey|key = %lu (with odd parity)\n",start+j);
-			//for(i=0;i<8;i++){
-			//	printf("%03u|%03u\n",randkey[i],key[i]); //printing bytes
-			//}
-			//printf("\n");
-			break; //found key so exit early
+	unsigned char *foundkey;
+	for(i=0;i<nthreads;i++){ //wait for the threads to join
+		int rt = pthread_join(threads[i],(void **)&foundkey); //wait for threads to finish
+		if(rt == 0){
+			if(foundkey != NULL){ //a solution is found
+				value = uchararray2lu(foundkey,BLOCK);
+				break; //get out of loop
+			}
 		}
 	}
 
 	if(value != -1){ //key not found
 		printf("1,");
 		printf("%lu,",value);
-		printf("%lu,",uchararray2lu(key,BLOCK));
+		printf("%lu,",uchararray2lu(foundkey,BLOCK));
 		for(i=0;i<8;i++){ //print the key out
-			printf("[%03u]",key[i]);
+			printf("[%03u]",foundkey[i]);
 		}
 		printf("\n");
 	}else{
 		printf("0,%lu,%lu,null\n",start,start+iterations-1);
 	}
 
+	free(foundkey); //free the key
+	free(threads);
+	free(allargs);
 	close(plaintextfd); //close the files
 	close(ciphertextfd);
 
